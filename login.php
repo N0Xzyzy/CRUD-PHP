1c431d5ddfe80e3bcaebd18b6d910b58be831b41
N0Xzyzy <nosssssrtrakzzz@gmail.com>
2025-05-29 07:44:14 +0700

    hai

--- login.php
+++ login.php
@@ -223,3 +223,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
 </body>
 </html>
+<?php
+session_start();
+include "koneksi.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = hash('sha256', $_POST['password']);
+
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        $_SESSION['login'] = true;
+        header("Location: index.php");
+        exit;
+    } else {
+        $error = "Login gagal!";
+    }
+}
+?>
+
+<form method="post">
+    <h2>Login</h2>
+    <?php if (isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
+    <input type="text" name="username" placeholder="Username" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit">Login</button>
+</form>
